65d645af4556a6a96e936b559503a4e7f1f85243
05AM <anytime0224@gmail.com>
2022-02-11 22:33:10 +0900

    todolist-vue-ver.1.0

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { ref } from 'vue';
+
+const items = ref([])
+
+function add() {
+
+  const todoText = document.getElementById('todo-input').value
+
+  console.log(items)
+  console.log(items.value)
+
+  items.value.push(todoText)
+
+  document.getElementById('todo-input').value = ''
+}
+
+function remove(itemsIndex) {
+  console.log('remove')
+
+  items.value.splice(itemsIndex, 1)
+}
+</script>
+
+<template>
+  <div class="todo-list">
+    <h1 class="todo-list-h1">Todo List</h1>
+    <div class="todo-list-add">
+      <input
+        type="text"
+        id="todo-input"
+        placeholder="add Items"
+        >
+        <button type="button" @click="add()">추가</button>
+    </div>
+    <div class="todo-items">
+      <div v-for="(item, index) in items" :key="index">
+        <div>{{ index + 1 }} - {{ item }}</div>
+        <button type="button" @click="remove(index)">제거</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  html, body {
+    padding: 0;
+    margin: 0;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .todo-list .todo-list-h1 {
+    font-size: 24px;
+  }
+
+  .todo-list .todo-list-add {
+    display: flex;
+  }
+
+  .todo-list .todo-items {
+    margin-top: 8px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .todo-list .todo-items > * {
+    display: flex;
+    justify-content: space-between;
+    padding: 4px;
+  }
+</style>
